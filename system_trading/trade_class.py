326355e67185fc6a718c34742dc98436f78d79d8
hzmzk <hazim_zaki96@yahoo.com>
2026-02-11 13:39:12 +0800

    a

--- system_trading/trade_class.py
+++ system_trading/trade_class.py
@@ -6,7 +6,7 @@ from util import datetime_csv
 
 class Stock:
     def __init__(self, ticker_symbol, start_date="", end_date=""):
-
+ 
         if(not ticker_symbol + ".csv" in os.listdir("price_data/")):
             price =  yf.download([ticker_symbol], period="max", auto_adjust=True, progress=False)["Close"]
             price.to_csv("price_data/" + stock + ".csv")
