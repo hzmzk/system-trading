de6b2f72db7be5b1d87d28145057e200a37af3fe
hzmzk <hazim_zaki96@yahoo.com>
2026-04-01 13:08:17 +0800

    minor changes

--- system_trading/tws.py
+++ system_trading/tws.py
@@ -1,31 +1,40 @@
 import pandas as pd
 
+from datetime import datetime
+
 from ib_async import *
 
 ib = IB()
 ib.connect('127.0.0.1', 4002, clientId=1)
 
-def create_ib_price(ticker, period):
+def create_stock_data(ticker):
     
     contract = Stock(ticker, "SMART", "USD")
 
-    ib.reqHeadTimeStamp(contract, whatToShow="TRADES", useRTH=True)
+    max_period = str(datetime.now().year - ib.reqHeadTimeStamp(contract, 'ADJUSTED_LAST', True).year + 1) + ' Y'
 
     bar = ib.reqHistoricalData(
         contract,
         endDateTime="",
-        durationStr=period,
+        durationStr=max_period,
         barSizeSetting="1 day",
-        whatToShow="TRADES",
+        whatToShow="ADJUSTED_LAST",
         useRTH=True,
-        formatDate=1,
+        formatDate=1
     )
 
     df = util.df(bar)
-
-    datetime_index = pd.DatetimeIndex(df["date"], name="Date")
     
-    df = df.drop("date", axis="columns")
-    df = df.set_index(datetime_index)
+    datetime_index = pd.DatetimeIndex(df["date"], name="Date")
 
-    return df
+    df = df.drop("date", axis="columns")
+
+    parameter = ["Open", "High", "Low", "Close", "Volume", "Average", "BarCount"]
+
+    parameter_column = pd.MultiIndex.from_product([parameter,[ticker]], names=['Price','Ticker'])
+
+    data = df[df.columns].apply(pd.to_numeric).to_numpy()
+
+    df = pd.DataFrame(data, index=datetime_index, columns=parameter_column)
+ 
+    df.to_csv('ib_price_data/' + ticker + '.csv')
